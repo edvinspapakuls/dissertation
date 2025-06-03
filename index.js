4f4c4cc92b80d604514ffb42182cea7cf6f3e632
edvinspapakuls <47062521+edvinspapakuls@users.noreply.github.com>
2025-06-03 21:31:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -22,19 +22,10 @@ app.use(cors({
 }));
 
 //manage user sessions
-const Redis = require('ioredis');
-const RedisStore = require('connect-redis')(session);
-const redisClient = new Redis('redis://default:hYEJmTXrqDgQdMSWdSSYM7abZUMVROWh@redis-13204.c338.eu-west-2-1.ec2.redns.redis-cloud.com:13204');
 app.use(session({
-  store: new RedisStore({ client: redisClient }),
   secret: 'gdrive-onedrive',
   resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'None'
-  }
+  saveUninitialized: true
 }));
 
 app.use(passport.initialize());
